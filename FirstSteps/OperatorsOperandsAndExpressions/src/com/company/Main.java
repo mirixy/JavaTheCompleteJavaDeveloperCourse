e8f8f1299a9340e6c4a75dc0448bdb5e923ab281
Miriam <miriamxy@outlook.com>
2021-01-04 20:40:38 +0100

    Finished all the Videos in Section 3. - First Steps

--- FirstSteps/OperatorsOperandsAndExpressions/src/com/company/Main.java
+++ FirstSteps/OperatorsOperandsAndExpressions/src/com/company/Main.java
@@ -53,10 +53,57 @@ public class Main {
         if (topScore < 100){
             System.out.println("You got the high score!, mate");
         }
-        int secondTopScore = 79;
-        if (topScore > secondTopScore && topScore < 100){
+        int secondTopScore = 81;
+        if ((topScore > secondTopScore) && (topScore < 100)){
             System.out.println("Greater than second top score and less than 100");
         }
+
+        if ((topScore > 90) || (secondTopScore <= 90)){
+            System.out.println("Either or both of the conditions are true!");
+        }
+
+        // Challenge, well Not a Challenge for me. q.q Boring stuff in the beginning that I did in other programming
+        //  languages too...
+        int newValue = 50;
+        if (newValue == 50){ // The Equal signs have to be == otherwise it will treat it as an Integer, which can't be in an if statement, I think.
+            System.out.println("This is true");
+        }
+
+        boolean isCar = false;
+        if (isCar == true){ // There was only one = sign, so it sets the boolean to true and not comparing it.
+            System.out.println("This is not supposed to happen");
+        }
+        isCar = true;
+        boolean wasCar = isCar ? true : false;
+        if(wasCar){
+            System.out.println("wasCar is true");
+        }
+
+        /* The ternary operator "?" is a shortcut to assigning one of two values to a variable depending on
+            a given condition.
+
+            It's a shortcut of the if-then-else statement
+         */
+
+        int ageOfClient = 20;
+        boolean isEighteenOrOver = ageOfClient == 20 ? true : false;
+        /* Operand one - ageOfClient == 20 in this case is the condition we are checking. It needs to return true or false.
+            Operand two - true here is the value to assign to the variable isEighteenOrOver if the condition is true
+            Operand three - false here is the value to assign to the variable isEighteenOver if the condition was false.
+         */
+
+
+        // Operator Challenge
+        double myDouble = 20.00d;
+        double mySecondDouble = 80.00d;
+        double myresult = (myDouble + mySecondDouble) * 100.00d;
+        double myRemainder = myresult % 40.00d;
+        boolean isResultTrue = (myRemainder == 0) ? true:false;
+        System.out.println("Is the remainder 0 ? " + isResultTrue);
+        if (!isResultTrue){
+            System.out.println("Got some remainder " + myRemainder);
+        }
+
 
 
     }
