2e48ff7ff2eab548394b2171a73c55665ab8de45
Miriam <miriamxy@outlook.com>
2021-01-04 21:14:44 +0100

    Begin the next part of the course

--- Section 4/KeyWordsAndExpressions/src/com/company/Main.java
+++ Section 4/KeyWordsAndExpressions/src/com/company/Main.java
@@ -3,6 +3,14 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
+        // a mile is equal to 1.609344 kilometres
+        double kilometres = (100 * 1.609344); // kilometres = something is an expression! double is an data type
+        int highscore = 50;
+        if (highscore == 50 ){
+            System.out.println("This is an expression, too");
+        }
+
+
 
 
     }
