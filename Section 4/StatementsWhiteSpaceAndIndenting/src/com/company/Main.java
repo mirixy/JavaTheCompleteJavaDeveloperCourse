48887ea84305664c6421287da4f71b6d7fdc0786
Miriam <miriamxy@outlook.com>
2021-01-05 15:56:31 +0100

    Statements Whitespace and Indenting Finished

--- Section 4/StatementsWhiteSpaceAndIndenting/src/com/company/Main.java
+++ Section 4/StatementsWhiteSpaceAndIndenting/src/com/company/Main.java
@@ -3,6 +3,29 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    /* A statement is the complete Line as shown below */
+        int myVariable = 50;
+        myVariable++;
+        myVariable--;
+        System.out.println("This is a test");
+
+
+        // Statements don't need to be one line
+        System.out.println("This is " +
+                "another " +
+                "still more.");
+
+	    int anotherVariable = 50; myVariable--; System.out.println("This is another one"); // This style is not that readable
+
+        /* Concept of Whitespace */
+        // Spaces are good to clarify things
+        // Indenting is also better for readability
+        if(myVariable == 50) {
+            System.out.println("Printed");
+        }
+
+
+
+
     }
 }
