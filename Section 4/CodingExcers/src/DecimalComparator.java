7d59f523b9ec87d4ea91270f5ca6fd78f051decc
Miriam <miriamxy@outlook.com>
2021-01-06 16:42:21 +0100

    Finally found my own Solution to comparing Strings.

--- Section 4/CodingExcers/src/DecimalComparator.java
+++ Section 4/CodingExcers/src/DecimalComparator.java
@@ -1,15 +1,25 @@
 
 public class DecimalComparator {
-
-    public static boolean areEqualByThreeDecimalPlaces(double numberOne, double numberTwo){
-            double difference = Math.abs((double) (numberOne - numberTwo));
-
-            return (difference < 0.0009);
-
+    public static boolean areEqualByThreeDecimalPlaces(double numberOne, double numberTwo) {
+        String str = String.valueOf(numberOne);
+        String str2 = String.valueOf(numberTwo);
+        String result1 = str.substring(0,5);
+        String result2 = str2.substring(0,5);
+        if(result1.equals(result2)){
+            System.out.println(result1 + " " + result2 );
+            return true;
+        }
+        else {  return false; }
 
 
 
     }
+    /*public static boolean areEqualByThreeDecimalPlaces(double numberOne, double numberTwo){
+            double difference = Math.abs((double) (numberOne - numberTwo));
+
+            return (difference < 0.0009);
+    }*/
+
 }
 /*
     I need to learn this. How could I fail so hard ? I thought about using Arrays but couldn't figure out
