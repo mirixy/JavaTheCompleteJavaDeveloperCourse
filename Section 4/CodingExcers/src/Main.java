128ec76e2c0f7ef4d75afdad7588566a28cf7f2e
Miriam <miriamxy@outlook.com>
2021-01-06 12:13:27 +0100

    Coding Exercises

---
+++ Section 4/CodingExcers/src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+           SpeedConverter.printConversion(100);
+           MegaBytesConverter.printMegaBytesAndKiloBytes(2000);
+    }
+}
