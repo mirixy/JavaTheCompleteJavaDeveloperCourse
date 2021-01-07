8b3bff55c807294e4a2ea54a84fd29df039886f4
Miriam <miriamxy@outlook.com>
2021-01-07 22:11:05 +0100

    Challenge Solution

--- Section 4/SecondsAndMinutesChallenge/src/com/company/Main.java
+++ Section 4/SecondsAndMinutesChallenge/src/com/company/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println(getDurationString(61, 9));
+        System.out.println(getDurationString(160, 9));
         System.out.println(getDurationString(3600));
     }
 
@@ -18,16 +18,24 @@ public class Main {
 //       int secondsR = seconds % 60;
 //       int calcMinutes = calcSecondsToMinutes + minutes;
 
-        else if (seconds < 10 && minutes > 0  ) {
             int hours = minutes / 60;
             int minutesRemain = minutes % 60;
-            return hours + "h " + minutesRemain + "m 0" + seconds + "s";
-        }
-        else {
-            int hours = minutes / 60;
-            int minutesRemain = minutes % 60;
-            return hours + "h " + minutesRemain + "m " + seconds + "s";
-        }
+
+            String hoursString = hours + "h ";
+            if (hours < 10){
+                hoursString = "0" + hoursString;
+            }
+            String secondssString = seconds + "s";
+            if (seconds < 10){
+                secondssString = "0" + secondssString;
+            }
+            String minutessString = minutesRemain + "m ";
+            if (minutes < 10) {
+                minutessString = "0" + minutessString;
+            }
+
+            return hoursString  + minutessString  + secondssString ;
+
 
     }
     private static String getDurationString (int seconds) {
