f24784b93c7074367a9ed050e1d39ef1640fc721
Miriam <miriamxy@outlook.com>
2021-01-07 19:04:38 +0100

    Overloading Methods

--- Section 4/MethodOverloading/src/com/company/Main.java
+++ Section 4/MethodOverloading/src/com/company/Main.java
@@ -4,16 +4,21 @@ public class Main {
 
     public static void main(String[] args) {
         int newScore = calculateScore("Miriam", 500);
-	    calculateScore(500);
+	    calculateScore(75);
+	    calculateScore();
     }
 
     public static int calculateScore(String playerName, int score) {
-        System.out.println("Player " + playerName + " scored" + score + " points");
+        System.out.println("Player " + playerName + " scored " + score + " points");
         return score * 1000;
     }
     public static int calculateScore(int score) {
-        System.out.println("Player scored" + score + " points");
+        System.out.println("Unnamed Player scored " + score + " points");
         return score * 1000;
+    }
+    public static int calculateScore() {
+        System.out.println("No Player name, no player score");
+        return 0;
     }
 
 }
