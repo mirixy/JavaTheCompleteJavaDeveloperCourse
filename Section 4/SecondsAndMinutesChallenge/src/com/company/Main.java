e41bf9491ff22016ee87e585a4f40f4cb3ee82b8
Miriam <miriamxy@outlook.com>
2021-01-07 21:21:57 +0100

    Challenge Seconds and Minuts, was pretty easy to be honest.

--- Section 4/SecondsAndMinutesChallenge/src/com/company/Main.java
+++ Section 4/SecondsAndMinutesChallenge/src/com/company/Main.java
@@ -4,7 +4,8 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        getDurationString(365, 150);
+        getDurationString(61, 0);
+        getDurationString(3600);
     }
 
     public static int getDurationString (int minutes, int seconds) {
@@ -15,13 +16,25 @@ public class Main {
 
 
         int calcSecondsToMinutes = seconds / 60;
-        int secondsRemain = seconds % 60;
+        int secondsR = seconds % 60;
         int calcMinutes = calcSecondsToMinutes + minutes;
         int hours = calcMinutes / 60;
         int minutesRemain = calcMinutes % 60;
-        System.out.println(hours + "h " + minutesRemain + "m " + secondsRemain + "s");
+        if (secondsR == 0 && minutesRemain > 0 && hours > 0 ){
+            System.out.println(hours + "h " + minutesRemain + "m " + "00s");
+        }
+        else
+        System.out.println(hours + "h " + minutesRemain + "m " + secondsR + "s");
         return 0;
 
-
+    }
+    public static int getDurationString (int seconds) {
+        if ( seconds < 0) {
+            System.out.println("Invalid value");
+            return -1;
+        }
+        int minutes = seconds / 60;
+        int secondsR = seconds % 60;
+        return getDurationString(minutes, secondsR);
     }
 }
