4c9c9edc13d5322c7c271ea45c6b04ab2fa4289e
Miriam <miriamxy@outlook.com>
2021-01-09 12:41:22 +0100

    Changed Folder

--- Section 4/CodingExcers/src/Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-           SpeedConverter.printConversion(100);
-           MegaBytesConverter.printMegaBytesAndKiloBytes(2000);
-           LeapYear.isLeapYear(1924);
-           DecimalComparator.areEqualByThreeDecimalPlaces(3.1756, 3.175);
-    }
-}
